18417a015accb0f02fd2ed0797c39fa46b095dfc
zt247206112 <247206112@qq.com>
2020-03-29 19:57:58 +0800

    添加基础属性支持

--- js/input_plug-in.js
+++ js/input_plug-in.js
@@ -9,22 +9,23 @@
 		el: '',
 		placeholder: '',
 		maxlength: '',
-		disable: false,
+		disabled: false,
 		change: function () {}
 	}
 	// 创建模拟的input
 	initPlugIn.prototype.creatInput = function () {
 		let _this = this;
 		let inputDom = document.createElement('div');
-		inputDom.setAttribute("contenteditable", "true");
+		inputDom.setAttribute("contenteditable", !this.opts.disabled);
 		inputDom.setAttribute('placeholder', this.opts.placeholder);
-		inputDom.setAttribute('disable', this.opts.disable);
 		inputDom.className = 'plugin-box';
 		// 变相添加div的change事件
 		inputDom.addEventListener('keyup', function (e) {
+			let ev = e || window.event
 			if (_this.opts.maxlength) {
 				// 用户设置了maxlength属性
 				inputDom.innerText = inputDom.innerText.substring(0, parseInt(_this.opts.maxlength));
+				_this.keepLastIndex(ev.target);
 			}
 			_this.opts.change(inputDom.innerText);
 		});
@@ -52,5 +53,20 @@
 		    }
 		})
 		this.opts.el.appendChild(inputDom);
+	}
+	// 将光标移向最后一位
+	initPlugIn.prototype.keepLastIndex = function (obj) {
+	    if (window.getSelection) { //ie11 10 9 ff safari
+	        obj.focus(); //解决ff不获取焦点无法定位问题
+	        let range = window.getSelection(); //创建range
+	        range.selectAllChildren(obj); //range 选择obj下所有子内容
+	        range.collapseToEnd(); //光标移至最后
+	    } else if (document.selection) { //ie10 9 8 7 6 5
+	        let range = document.selection.createRange(); //创建选择对象
+	        //var range = document.body.createTextRange();
+	        range.moveToElementText(obj); //range定位到obj
+	        range.collapse(false); //光标移至最后
+	        range.select();
+	    }
 	}
 })(window)
